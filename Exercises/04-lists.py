1e5e0a5df42df127eac57e935633b829ab569fea
Alexander Perez <alexperez4016@gmail.com>
2023-12-26 16:46:53 -0300

    Practice Lists

---
+++ Exercises/04-lists.py
@@ -0,0 +1,40 @@
+### Exercises: Level 1###
+# 1.Declare an empty list
+
+# 2.Declare a list with more than 5 items
+
+# 3.Find the length of your list
+
+# 4.Get the first item, the middle item and the last item of the list
+
+# 5.Declare a list called mixed_data_types, put your(name, age, height, marital status, address)
+
+# 6.Declare a list variable named it_companies and assign initial values Facebook, Google, Microsoft, Apple, IBM, Oracle and Amazon.
+
+# 7.Print the list using print()
+
+# 8.Print the number of companies in the list
+
+# 9.Print the first, middle and last company
+
+# 10.Print the list after modifying one of the companies
+
+# 11.Add an IT company to it_companies
+
+# 12.Insert an IT company in the middle of the companies list
+
+# 13.Change one of the it_companies names to uppercase (IBM excluded!)
+
+Join the it_companies with a string '#;  '
+
+Check if a certain company exists in the it_companies list.
+
+Sort the list using sort() method
+
+Reverse the list in descending order using reverse() method
+
+Slice out the first 3 companies from the list
+
+Slice out the last 3 companies from the list
+
+Slice out the middle IT company or companies from the list
