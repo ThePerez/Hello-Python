790dc7dc0d93454b91c362934924ba5f31a7afed
Alexander Perez <alexperez4016@gmail.com>
2024-03-12 14:58:14 -0300

    new

---
+++ Theory/08 conditionals.py
@@ -0,0 +1,19 @@
+### Conditionals ###
+
+my_condition = False
+
+if my_condition:   #   Es lo mismo que if my_condition == True :
+    print("Se ejecuta el if")
+    
+my_condition = 5 * 2
+
+if my_condition == 10:
+    print("Se ejecuta el segundo if")
+    
+if my_condition >= 10:
+    print("Es mayor o igual a 10")
+else:
+    print("Es menor a 10")    
+
+    
+print("La ejecución continúa")
